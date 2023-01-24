aa7d3f0282a16071a259793a77298e3d80174c8d
TAKAMURAFUMIHITO <108682238+TAKAMURAFUMIHITO@users.noreply.github.com>
2023-01-24 16:18:30 +0900

    api作成(未検証)

--- src/index.ts
+++ src/index.ts
@@ -15,21 +15,90 @@ AppDataSource
 const app = express();
 app.use(express.json());
 
+const bookRepository = AppDataSource.getRepository(Book)
 
-app.get("/books", function (req: Request, res: Response) {
+// GET /books
+app.get("/books", async function (req: Request, res: Response) {
+    try {
+        const books = await bookRepository.find();
+        res.json(books);
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error);
+    };
 });
 
-app.get("/books/:id", function (req: Request, res: Response) {
+// GET /books/:id
+app.get("/books/:id", async function (req: Request, res: Response) {
+    try {
+        const book = await bookRepository.findOneBy({
+            id: Number(req.params.id),
+        });
+        if (book == null) {
+            res.status(204).send();
+            return;
+        };
+        res.send(book);
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error);
+    };
 });
 
-app.post("/books", function (req: Request, res: Response) {
+// POST /contents
+app.post("/books", async function (req: Request, res: Response) {
+    try {
+        const book = new Book(req.body.title, req.body.body);
+        await bookRepository.save(book);
+        res.send(book);
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error);
+    };
 });
 
-app.put("/books/:id", function (req: Request, res: Response) {
+// PUT /contents/:id
+app.put("/books/:id", async function (req: Request, res: Response) {
+    try {
+        const book = await bookRepository.findOneBy({
+            id: Number(req.params.id),
+        });
+        if (book == null) {
+            res.status(404).send();
+            return;
+        };
+
+        // 一部更新も可能にする
+        book.title = req.body.title || book.title;
+        book.body = req.body.body || book.body;
+
+        await bookRepository.save(book);
+        res.send(book);
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error);
+    };
 });
 
-app.delete("/books/:id", function (req: Request, res: Response) {
+// DELETE /contents/:id
+app.delete("/books/:id", async function (req: Request, res: Response) {
+    try {
+        const content = await bookRepository.findOneBy({
+            id: Number(req.params.id),
+        });
+        if (content == null) {
+            res.status(404).send();
+            return;
+        };
+
+        await bookRepository.remove(content);
+        res.sendStatus(404).send();
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error);
+    };
 });
 
-
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('App listening on port 3000.');
+});
